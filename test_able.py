671d17aaa6b32f8c10ef849eee6305c4bfd88906
diego <jdrozosajonia@gmail.com>
2025-04-11 23:42:15 -0500

    function tabla has been changed as defTabla to avoid confusions and print lines does not exists anymore, so i added an acumulative string to return it, doing the function more flex

--- test_able.py
+++ test_able.py
@@ -5,18 +5,18 @@ def generar_diccionario(num_claves, longitud_listas):
     diccionario = {}
     
     for _ in range(num_claves):
-        # Generar un nombre aleatorio para la clave
         nombre_random = ''.join(random.choices(string.ascii_letters, k=8))  # 8 letras al azar
-        # Generar una lista de números flotantes con la longitud dada
         lista_flotantes = [round(random.uniform(0, 100), 30) for _ in range(longitud_listas)]
-        # Agregar al diccionario
         diccionario[nombre_random] = lista_flotantes
     
     return diccionario
 
-# Generar un diccionario con 5 claves, y listas de tamaño 10
-resultado = generar_diccionario(num_claves=8, longitud_listas=9)
+# Generar un diccionario dadas x, y dimensiones
+entrada=input("set dimension | x | y |: ").split(" ")
+entrada=list(map(int,entrada))
+print(entrada)
+resultado = generar_diccionario(num_claves=entrada[0], longitud_listas=entrada[1])
 
 print(resultado)
-table.tabla(resultado)
-#NOTA: este script fue creado con ChatGPT
+tabla=table.getTabla(resultado)
+print(tabla)
